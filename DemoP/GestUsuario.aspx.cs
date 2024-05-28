dd63d0b20f74b40171be33aed0f5f10af6e6d305
QuesadillaHot0322 <alberto.alapisco@uabc.edu.mx>
2024-05-28 09:30:42 -0700

    ya jala el inicio de sesion
    solo funciona con los usuarios registrados

--- DemoP/GestUsuario.aspx.cs
+++ DemoP/GestUsuario.aspx.cs
@@ -1,106 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Data;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using System.Data.SqlClient;
+    using System.Data;
 
-namespace DemoP
-{
-    public partial class GestUsuario : System.Web.UI.Page
+    namespace DemoP
     {
-        protected void Page_Load(object sender, EventArgs e)
+        public partial class GestUsuario : System.Web.UI.Page
         {
-            if (!IsPostBack)
+            protected void Page_Load(object sender, EventArgs e)
             {
-                BindGridView();
-            }
-        }
-
-        private void BindGridView()
-        {
-            string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
-            string query = "SELECT * FROM Usuarios";
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand(query, con))
+                if (!IsPostBack)
                 {
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        GridView1.DataSource = dt;
-                        GridView1.DataBind();
-                    }
-                }
-            }
-        }
-
-        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-            int userID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
-
-            string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
-            string query = "DELETE FROM Usuarios WHERE ID=@UserID";
-
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("@UserID", userID);
-
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
+                    BindGridView();
                 }
             }
 
-            BindGridView(); 
-        }
-
-        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
-        {
-            GridView1.EditIndex = e.NewEditIndex;
-            BindGridView();
-        }
-
-        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
-        {
-            GridView1.EditIndex = -1;
-            BindGridView();
-        }
-
-        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
-        {
-            try {
-                int userID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
-                TextBox txtNombre = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtNombre");
-                TextBox txtMatricula = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtMatricula");
-                TextBox txtCorreo = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtCorreo");
-                TextBox txtContraseña = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtContraseña");
-                TextBox txtPermisos = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtPermisos");
-
-                string nombre = txtNombre.Text;
-                string matricula = txtMatricula.Text;
-                string correo = txtCorreo.Text;
-                string contraseña = txtContraseña.Text;
-                string permisos = txtPermisos.Text;
-
+            private void BindGridView()
+            {
                 string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
-                string query = "UPDATE Usuarios SET Nombre=@Nombre, Matricula=@Matricula, Correo=@Correo, Contraseña=@Contraseña, Permisos=@Permisos WHERE ID=@UserID";
+                string query = "SELECT * FROM Usuarios";
 
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     using (SqlCommand cmd = new SqlCommand(query, con))
                     {
-                        cmd.Parameters.AddWithValue("@Nombre", nombre);
-                        cmd.Parameters.AddWithValue("@Matricula", matricula);
-                        cmd.Parameters.AddWithValue("@Correo", correo);
-                        cmd.Parameters.AddWithValue("@Contraseña", contraseña);
-                        cmd.Parameters.AddWithValue("@Permisos", permisos);
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+
+            protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+            {
+                int userID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+                string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
+                string query = "DELETE FROM Usuarios WHERE ID=@UserID";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
                         cmd.Parameters.AddWithValue("@UserID", userID);
 
                         con.Open();
@@ -109,19 +58,70 @@ namespace DemoP
                     }
                 }
 
-                GridView1.EditIndex = -1;
+                BindGridView(); 
+            }
+
+            protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+            {
+                GridView1.EditIndex = e.NewEditIndex;
                 BindGridView();
             }
-            finally
+
+            protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
             {
                 GridView1.EditIndex = -1;
                 BindGridView();
             }
-            }
 
-        protected string GetPasswordDisplay(string password)
-        {
-            return new string('*', password.Length);
+            protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+            {
+                try {
+                    int userID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+                    TextBox txtNombre = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtNombre");
+                    TextBox txtMatricula = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtMatricula");
+                    TextBox txtCorreo = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtCorreo");
+                    TextBox txtContraseña = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtContraseña");
+                    TextBox txtPermisos = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtPermisos");
+
+                    string nombre = txtNombre.Text;
+                    string matricula = txtMatricula.Text;
+                    string correo = txtCorreo.Text;
+                    string contraseña = txtContraseña.Text;
+                    string permisos = txtPermisos.Text;
+
+                    string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
+                    string query = "UPDATE Usuarios SET Nombre=@Nombre, Matricula=@Matricula, Correo=@Correo, Contraseña=@Contraseña, Permisos=@Permisos WHERE ID=@UserID";
+
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand(query, con))
+                        {
+                            cmd.Parameters.AddWithValue("@Nombre", nombre);
+                            cmd.Parameters.AddWithValue("@Matricula", matricula);
+                            cmd.Parameters.AddWithValue("@Correo", correo);
+                            cmd.Parameters.AddWithValue("@Contraseña", contraseña);
+                            cmd.Parameters.AddWithValue("@Permisos", permisos);
+                            cmd.Parameters.AddWithValue("@UserID", userID);
+
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                        }
+                    }
+
+                    GridView1.EditIndex = -1;
+                    BindGridView();
+                }
+                finally
+                {
+                    GridView1.EditIndex = -1;
+                    BindGridView();
+                }
+                }
+
+            protected string GetPasswordDisplay(string password)
+            {
+                return new string('*', password.Length);
+            }
         }
     }
-}
--- DemoP/ReUsuarios.aspx.cs
+++ DemoP/ReUsuarios.aspx.cs
@@ -1,35 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data;
 using System.Data.SqlClient;
-using System.Web.Configuration;
-using System.Web.Mvc;
 using System.Text.RegularExpressions;
 
 namespace DemoP
 {
     public partial class ReUsuarios : System.Web.UI.Page
     {
-        SqlConnection coneccion;
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void TextBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         protected void Guardar_Click(object sender, EventArgs e)
         {
             string nombre = nameU.Text;
@@ -39,13 +15,13 @@ namespace DemoP
             bool permiso = permiU.Checked;
 
             string query = "INSERT INTO Usuarios (Nombre, Matricula, Correo, Contraseña, Permisos) " +
-                "VALUES (@Nombre, @Matricula, @Correo, @Contraseña, @Permisos)";
+                           "VALUES (@Nombre, @Matricula, @Correo, @Contraseña, @Permisos)";
 
-            using (coneccion = new SqlConnection("Data Source=localhost;Initial Catalog=Queso;Integrated Security=True"))
+            string connectionString = "Data Source=localhost;Initial Catalog=Queso;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                coneccion.Open();
-
-                using (SqlCommand command = new SqlCommand(query, coneccion))
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Nombre", nombre);
                     command.Parameters.AddWithValue("@Matricula", matricula);
@@ -53,30 +29,34 @@ namespace DemoP
                     command.Parameters.AddWithValue("@Contraseña", contra);
                     command.Parameters.AddWithValue("@Permisos", permiso);
 
-                    if (Regex.IsMatch(matricula, @"^\d+$"))
+                    try
                     {
-                        try
-                        {
-                            int rowsAffected = command.ExecuteNonQuery();
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
 
-                            if (rowsAffected > 0)
-                            {
-                                ClientScript.RegisterStartupScript(this.GetType(), "registroExitoso", "alert('Registro exitoso');", true);
-                                Response.Redirect("GestUsuarios.aspx");
-                            }
-                            else
-                            {
-                                ClientScript.RegisterStartupScript(this.GetType(), "sinFilasInsertadas", "alert('No se insertaron datos en algunas filas');", true);
-                            }
+                        if (rowsAffected > 0)
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "registroExitoso", "alert('Registro exitoso');", true);
                         }
-                        catch (Exception ex)
+                        else
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "sinFilasInsertadas", "alert('No se insertaron datos en algunas filas');", true);
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (ex.Number == 2627) // Error de clave duplicada
                         {
                             ClientScript.RegisterStartupScript(this.GetType(), "error", "alert('Error: ID EXISTENTE');", true);
                         }
+                        else
+                        {
+                            ClientScript.RegisterStartupScript(this.GetType(), "error", $"alert('Error: {ex.Message}');", true);
+                        }
                     }
-                    else
+                    finally
                     {
-                        ClientScript.RegisterStartupScript(this.GetType(), "error", "alert('Error: SOLO SE ACEPTAN NUMEROS EN ID');", true);
+                        connection.Close();
                     }
                 }
             }
